865a23aedacc8e395e7d8c09e2474a69cca1a3c9
Yanislav Manshoo Pichugin <zortapex@email.com>
2023-08-15 18:25:43 +0300

    Понаделал ГИТа, новая колонка картинок

--- Twitter/Twitter/models.py
+++ Twitter/Twitter/models.py
@@ -5,7 +5,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-
-	post_text = models.TextField()
+	
+	post_thumbs = models.CharField(max_length=255, blank=True, default="/")
+	post_text = models.TextField(blank=True, default="")
 	post_author = models.ForeignKey(User, on_delete=models.CASCADE)
 	post_date = models.DateTimeField("date published")
--- Twitter/Twitter/views.py
+++ Twitter/Twitter/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import loader
+from django.template.response import TemplateResponse
 
 from .models import Post
 
 def index(request):
+	post_thumbs_field = "post_thumbs"
+	post_obj = Post.objects.first()
+	post_thumbs = getattr(post_obj, post_thumbs_field).split(";")
+
 	latest_posts = Post.objects.order_by("-post_date")[:5]
-	context = {"latest_posts": latest_posts}
+	context = {"latest_posts": latest_posts, "post_thumbs_as_images": post_thumbs}
 	return render(request, "twitter/twitter_indexPage.html", context)
+
+def postThumbsTuple(request, template_name="twitter/twitter_post.html"):
+	post_thumbs_field = "post_thumbs"
+	post_obj = Post.objects.first()
+	post_thumbs = getattr(post_obj, post_thumbs_field).split(";")
+
+	context = {"post_thumbs_as_images": post_thumbs}
+	
+	return TemplateResponse(request, template_name, context)
