30bb088595beef14ffae9d64a477462bebb8de6d
Yanislav Manshoo Pichugin <zortapex@email.com>
2024-01-08 01:38:27 +0300

    литтл баг с анонимус узер хе)

--- azzb/customUser/templatetags/show_posts.py
+++ azzb/customUser/templatetags/show_posts.py
@@ -6,9 +6,12 @@ register = template.Library()
 
 @register.inclusion_tag(filename='post/post.html', name="show_posts", takes_context=True)
 def show_posts(context):
-	post = context["post"]
-	request = context["request"]
-	context["liked"] = False
-	if post.likes.filter(pk=request.user.profile.id).exists():
-		context["liked"] = True
-	return context
+	try:
+		post = context["post"]
+		request = context["request"]
+		context["liked"] = False
+		if post.likes.filter(pk=request.user.profile.id).exists():
+			context["liked"] = True
+		return context
+	except:
+		return context
